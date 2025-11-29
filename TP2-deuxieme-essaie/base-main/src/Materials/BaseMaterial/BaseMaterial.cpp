cb32336006b6907cd3f8f3510e02e4ebd6e2c6a4
Frederick <Frederick.Taylor@usherbrooke.ca>
2025-11-29 15:11:12 -0500

    TP2 - 2 eletric boogaloo

---
+++ TP2-deuxieme-essaie/base-main/src/Materials/BaseMaterial/BaseMaterial.cpp
@@ -0,0 +1,54 @@
+
+#include "BaseMaterial.h"
+#include "Node.h"
+#include <glm/gtc/type_ptr.hpp>
+
+BaseMaterial::BaseMaterial(std::string name) : MaterialGL(name) {
+
+    vp = new GLProgram(MaterialPath + "BaseMaterial/Main-VS.glsl", GL_VERTEX_SHADER);
+    fp = new GLProgram(MaterialPath + "BaseMaterial/Main-FS.glsl", GL_FRAGMENT_SHADER);
+
+    m_ProgramPipeline->useProgramStage(vp, GL_VERTEX_SHADER_BIT);
+    m_ProgramPipeline->useProgramStage(fp, GL_FRAGMENT_SHADER_BIT);
+
+    l_View = glGetUniformLocation(vp->getId(), "View");
+    l_Proj = glGetUniformLocation(vp->getId(), "Proj");
+    l_Model = glGetUniformLocation(vp->getId(), "Model");
+
+    l_time = glGetUniformLocation(vp->getId(), "u_time");
+}
+
+BaseMaterial::~BaseMaterial() {}
+
+void BaseMaterial::render(Node *o) {
+
+    m_ProgramPipeline->bind();
+
+    o->drawGeometry(GL_TRIANGLES);
+    m_ProgramPipeline->release();
+}
+
+void BaseMaterial::animate(Node *o, const float elapsedTime) {
+
+    /**********************************************
+    TP 2 - A completer
+    Calculer et Transmettre les matrices Model View et Proj au shaders
+    - Utilisez glm::value_ptr(mat) pour trouver le pointeur de la matrice mat a transmettre au GPU via la fonction glProgramUniform*()
+    - Une matrice 4X4 se transmet grace a glProgramUniformMatrix4fv
+    ***********************************************/
+
+    if (elapsedTime < 10) m_time += elapsedTime;
+
+    glm::mat4 Projection = scene->camera()->getProjectionMatrix();
+    glm::mat4 View = scene->camera()->getViewMatrix();
+    glm::mat4 Model = o->frame()->getModelMatrix();
+
+    const GLuint vpId = vp->getId();
+    if (l_Proj >= 0) glProgramUniformMatrix4fv(vpId, l_Proj, 1, GL_FALSE, glm::value_ptr(Projection));
+    if (l_View >= 0) glProgramUniformMatrix4fv(vpId, l_View, 1, GL_FALSE, glm::value_ptr(View));
+    if (l_Model >= 0) glProgramUniformMatrix4fv(vpId, l_Model, 1, GL_FALSE, glm::value_ptr(Model));
+
+
+    if (l_time >= 0) glProgramUniform1f(vpId, l_time, m_time);
+
+}
